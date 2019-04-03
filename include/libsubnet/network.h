4d202aa06573f44930b54c1793f9b6158ace1832
Jon Palmisciano <jonpalmisc@icloud.com>
2019-04-02 23:05:28 -0400

    Adjusted multiline comment style

--- include/libsubnet/network.h
+++ include/libsubnet/network.h
@@ -26,15 +26,19 @@
 
 #include <stdint.h>
 
-/** @brief Converts a network address/mask from dot notation into packed bytes. <b>Does not check for invalid input!</b>
+/**
+ * @brief Converts a network address/mask from dot notation into packed bytes. <b>Does not check for invalid input!</b>
  * @param s The address or mask to convert.
- * @return A byte-packed network address/mask. */
+ * @return A byte-packed network address/mask.
+ */
 uint32_t dot2bin(char *s);
 
-/** @brief Converts the binary/in-memory representation of network address or subnet mask into a dot notation string.
+/**
+ * @brief Converts the binary/in-memory representation of network address or subnet mask into a dot notation string.
  * <b>Does not check for invalid input!</b>
  * @param i The address or mask to convert.
- * @return A dot-notation string. */
+ * @return A dot-notation string.
+ */
 char *bin2dot(uint32_t i);
 
 #endif // LIBSUBNET_NETWORK_H
--- include/libsubnet/subnet.h
+++ include/libsubnet/subnet.h
@@ -25,67 +25,93 @@
 
 #include <stdint.h>
 
-/** @struct subnet
+/**
+ * @struct subnet
  * @brief Represents a subnet.
  * @var subnet::addr
  * The subnet's base address
  * @var subnet::mask
- * The subnet's mask */
+ * The subnet's mask
+ */
 typedef struct {
   uint32_t addr;
   uint32_t mask;
 } subnet;
 
-/** @brief Constructs a new subnet object from a network address and a subnet mask. <b>Returns NULL if invalid input is
+/**
+ * @brief Constructs a new subnet object from a network address and a subnet mask. <b>Returns NULL if invalid input is
  * received!</b>
  * @param address The network address for the subnet
  * @param mask The mask for the subnet
- * @return A new subnet object with the given parameters */
+ * @return A new subnet object with the given parameters
+ */
 subnet *subnet_new(char *address, char *mask);
 
-/** @brief Constructs a new subnet object from a CIDR notation. <b>Returns NULL if invalid input is received!</b>
+/**
+ * @brief Constructs a new subnet object from a CIDR notation. <b>Returns NULL if invalid input is received!</b>
  * @param cidr The CIDR notation of the desired subnet
- * @return A new subnet object that can be represented by the given CIDR notation */
+ * @return A new subnet object that can be represented by the given CIDR notation
+ */
 subnet *subnet_new_cidr(char *cidr);
 
-/** @brief Returns the base address of a given subnet as a string in dot notation.
- * @return The base address as a string in dot notation */
+/**
+ * @brief Returns the base address of a given subnet as a string in dot notation.
+ * @return The base address as a string in dot notation
+ */
 char *subnet_addr(subnet *sn);
 
-/** @brief Returns the subnet mask of a given subnet as a string in dot notation.
- * @return The subnet mask as a string in dot notation */
+/**
+ * @brief Returns the subnet mask of a given subnet as a string in dot notation.
+ * @return The subnet mask as a string in dot notation
+ */
 char *subnet_mask(subnet *sn);
 
-/** @brief Returns the length of the subnet mask in bits.
- * @return The length of the subnet mask in bits */
+/**
+ * @brief Returns the length of the subnet mask in bits.
+ * @return The length of the subnet mask in bits
+ */
 int subnet_mask_length(subnet* sn);
 
-/** @brief Returns the CIDR notation of a given subnet.
- * @return The CIDR notation of the subnet, including the base address. */
+/**
+ * @brief Returns the CIDR notation of a given subnet.
+ * @return The CIDR notation of the subnet, including the base address.
+ */
 char *subnet_cidr(subnet *sn);
 
-/** @brief Returns the number of hosts (addresses) contained within the subnet.
- * @return The number of hosts contained within the subnet */
+/**
+ * @brief Returns the number of hosts (addresses) contained within the subnet.
+ * @return The number of hosts contained within the subnet
+ */
 int subnet_num_hosts(subnet *sn);
 
-/** @brief Returns the number of usable hosts (addresses) contained within the subnet.
- * @return The number of usable hosts contained within the subnet */
+/**
+ * @brief Returns the number of usable hosts (addresses) contained within the subnet.
+ * @return The number of usable hosts contained within the subnet
+ */
 int subnet_num_usable_hosts(subnet *sn);
 
-/** @brief Returns the lowest-numbered host within the bounds of the subnet.
- * @return The lowest-numbered host within the bounds of the subnet */
+/**
+ * @brief Returns the lowest-numbered host within the bounds of the subnet.
+ * @return The lowest-numbered host within the bounds of the subnet
+ */
 char *subnet_min_host(subnet *sn);
 
-/** @brief Returns the highest-numbered host within the bounds of the subnet.
- * @return The highest-numbered host within the bounds of the subnet */
+/**
+ * @brief Returns the highest-numbered host within the bounds of the subnet.
+ * @return The highest-numbered host within the bounds of the subnet
+ */
 char *subnet_max_host(subnet *sn);
 
-/** @brief Gets the network address of a given subnet and returns it as a string in dot notation.
- * @return The network address of the subnet in dot notation */
+/**
+ * @brief Gets the network address of a given subnet and returns it as a string in dot notation.
+ * @return The network address of the subnet in dot notation
+ */
 char *subnet_network_addr(subnet *sn);
 
-/** @brief Gets the broadcast address of a given subnet and returns it as a string in dot notation.
- * @return The broadcast address of the subnet in dot notation */
+/**
+ * @brief Gets the broadcast address of a given subnet and returns it as a string in dot notation.
+ * @return The broadcast address of the subnet in dot notation
+ */
 char *subnet_broadcast_addr(subnet *sn);
 
 #endif // LIBSUBNET_SUBNET_H
